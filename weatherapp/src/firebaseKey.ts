389187d6abd38b864eb9eac73e55ee72758048be
Logical_IT <>
2023-05-01 11:24:25 +0900

    파이어베이스 api key (환경변수)

---
+++ weatherapp/src/firebaseKey.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+// 파이어베이스 환경변수
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_authdomain,
+  // databaseURL: process.env.REACT_APP_databaseurl,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measuremenId: process.env.REACT_APP_measurementId,
+};
+
+export default firebaseConfig;
