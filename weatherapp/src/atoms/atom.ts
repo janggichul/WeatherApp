9ce0910e322ad218b58b1cde6dbd1ff33bb2ff1c
Logical_IT <>
2023-05-01 11:26:04 +0900

    recoil atoms file

---
+++ weatherapp/src/atoms/atom.ts
@@ -0,0 +1,75 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+export const EmailState = atom({
+  key: 'EmailState',
+  default: '',
+});
+
+export const ImageState = atom({
+  key: 'ImageState',
+  default: '',
+});
+
+export const RoleState = atom({
+  key: 'RoleState',
+  default: false,
+});
+
+export const NicknameState = atom({
+  key: 'NicknameState',
+  default: '',
+});
+
+export const GridXState = atom({
+  key: 'GridXState',
+  default: null,
+});
+export const GridYState = atom({
+  key: 'GridYState',
+  default: null,
+});
+export const CurrentYState = atom({
+  key: 'CurrentYState',
+  default: null,
+});
+export const CurrentXState = atom({
+  key: 'CurrentXState',
+  default: null,
+});
+export const hourTempaState = atom({
+  key: 'hourTempaState',
+  default: '',
+});
+
+export const precipitationState = atom({
+  key: 'precipitationState',
+  default: '',
+});
+
+export const isLoginState = atom({
+  key: 'isLoginState',
+  default: false,
+});
+
+export const AccessTokenState = atom({
+  key: 'AccessTokenState',
+  default: '',
+});
+
+export const TempaMinState = atom({
+  key: 'TempaMinState',
+  default: '',
+});
+
+export const TemapaMaxState = atom({
+  key: 'TempaMaxState',
+  default: '',
+});
+
+export const ProbabilityState = atom({
+  key: 'ProbabilityState',
+  default: '',
+});
