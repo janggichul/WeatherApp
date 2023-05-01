d70b70731e5b5e8d00ccb79e4b1a9ab1e6fae87b
Logical_IT <>
2023-05-01 11:30:41 +0900

    회원정보 수정 로직

---
+++ weatherapp/src/page/profile/InformationCorrection.tsx
@@ -0,0 +1,6 @@
+import InformationCorrectionCode from 'components/ProfileCode/InformationCorrectionCode';
+import React from 'react';
+
+export default function InformationCorrection() {
+  return <InformationCorrectionCode />;
+}
